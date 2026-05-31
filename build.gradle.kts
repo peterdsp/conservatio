a4f62982d0f29da1653e0d4ee799a0e5c491d61f
Petros Dhespollari <info@peterdsp.dev>
2026-05-31 08:51:47 +0300

    Declare kotlinJvm plugin in root and server for clean JVM build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.sqldelight) apply false
 }
